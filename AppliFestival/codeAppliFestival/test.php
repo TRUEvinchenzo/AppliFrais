d20e11c3a5b1fe29c04941c040f4ab61937eec96
kevenBZH <pedrono@B4-W7-15.sio.chaptalqper>
2016-10-05 16:29:17 +0200

    Y

--- AppliFestival/codeAppliFestival/test.php
+++ AppliFestival/codeAppliFestival/test.php
@@ -1,8 +1,5 @@
 <?php
 //CECI EST UN TEST DE TYPE LOL
 // Virez !!! NTMV
-<<<<<<< HEAD
 // REND L'ARGENT AU ABONNES !! CULAY !!!
-=======
 // YOOOOOOO TOUT LE MONDE, C'EST FATBOY ET AUJOURD'HUI, ON VA AU MACDO
->>>>>>> branch 'master' of https://github.com/TRUEvinchenzo/ftudthj.git
